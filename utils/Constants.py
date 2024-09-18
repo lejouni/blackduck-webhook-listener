970c8599139de212a44f7ac7b89b7aa507589d9b
Jouni Lehto <lehto.jouni@gmail.com>
2024-09-18 13:41:03 +0300

    Added also CNC

--- utils/Constants.py
+++ utils/Constants.py
@@ -1,11 +1,14 @@
 class Tools:
     BLACK_DUCK = "Black Duck"
     COVERITY = "Coverity"
+    CNC = "CNC"
 
 class SRMTools:
     BLACK_DUCK = "Black Duck"
     COVERITY = "Coverity"
+    CNC = "CNC"
 
 class GitHubTools:
     BLACK_DUCK = "Synopsys Black Duck Intelligent"
     COVERITY = "Coverity"
+    CNC = "CNC"
